c797f7633fdcfbda38ba50d8e5c5d38d4e26fe7d
VladStarostenko <vlad.starostenko@data-lake.co>
2022-10-24 12:00:02 +0200

    Blurring vesting schedule and withdraw tables if wallet is not connected

--- src/components/vestingOverview/vestingSchedule/VestingSchedule.tsx
+++ src/components/vestingOverview/vestingSchedule/VestingSchedule.tsx
@@ -1,19 +1,48 @@
+import { GradientButtonWithIcon } from '../../button/gradient/GradientButtonWithIcon';
 import { IVestingSchedule } from '../../../interfaces/vestingSchedule.interface';
 import { ScheduleTab } from './ScheduleTab';
+import { WalletConnectContext } from '../../../context';
+import keyIcon from './../../../assets/icons/key-icon.svg';
+import { useContext } from 'react';
 
 interface Props {
     data: IVestingSchedule[];
 }
 
-export const VestingSchedule = ({ data }: Props) => (
-    <div className="w-full h-full flex flex-col items-center bg-black-700 rounded-[30px] inset-shadow px-4">
-        <div className="w-full flex justify-start py-10 px-6">
-            <div className="font-kanit-medium color-gray-gradient text-shadow text-xl tracking-[.12em]">
-                YOUR $LAKE VESTING SCHEDULE
+export const VestingSchedule = ({ data }: Props) => {
+    const { account, activateProvider } = useContext(WalletConnectContext);
+
+    const activate = async () => {
+        await activateProvider();
+    };
+    return (
+        <div className="w-full h-full bg-black-700 rounded-[30px] inset-shadow relative">
+            <div
+                className={`w-full h-full flex flex-col items-center px-4 ${
+                    account ? '' : 'blur-sm'
+                }`}
+            >
+                <div className="w-full flex justify-start py-10 px-6">
+                    <div className="font-kanit-medium color-gray-gradient text-shadow text-xl tracking-[.12em]">
+                        YOUR $LAKE VESTING SCHEDULE
+                    </div>
+                </div>
+                <div className="w-full flex flex-col items-center justify-center">
+                    <ScheduleTab data={data} />
+                </div>
             </div>
+            {!account && (
+                <div className="absolute top-[50%] left-[37%]">
+                    <GradientButtonWithIcon
+                        size="medium"
+                        disabled={false}
+                        text="CONNECT WALLET"
+                        onClick={activate}
+                    >
+                        <img src={keyIcon} alt="key"></img>
+                    </GradientButtonWithIcon>
+                </div>
+            )}
         </div>
-        <div className="w-full flex flex-col items-center justify-center">
-            <ScheduleTab data={data} />
-        </div>
-    </div>
-);
+    );
+};
--- src/components/vestingOverview/withdraw/Withdraw.tsx
+++ src/components/vestingOverview/withdraw/Withdraw.tsx
@@ -1,18 +1,22 @@
 import { useContext, useEffect, useState } from 'react';
-import styled from 'styled-components';
-import logo from '../../../assets/icons/logo.svg';
-import { colors } from '../../../constants/colors';
-import { WalletConnectContext } from '../../../context';
-import { useTokenClaim } from '../../../hooks/use-token-claim';
-import { IVestingSchedule } from '../../../interfaces/vestingSchedule.interface';
+
 import { GradientButton } from '../../button/gradient/GradientButton';
+import { GradientButtonWithIcon } from '../../button/gradient/GradientButtonWithIcon';
+import { IVestingSchedule } from '../../../interfaces/vestingSchedule.interface';
+import { WalletConnectContext } from '../../../context';
+import { colors } from '../../../constants/colors';
+import keyIcon from './../../../assets/icons/key-icon.svg';
+import logo from '../../../assets/icons/logo.svg';
+import styled from 'styled-components';
+import { useTokenClaim } from '../../../hooks/use-token-claim';
 
 interface Props {
     data: IVestingSchedule[];
 }
 
 export const Withdraw = ({ data }: Props) => {
-    const { library } = useContext(WalletConnectContext);
+    const { library, account, activateProvider } =
+        useContext(WalletConnectContext);
     const [totalLocked, setTotalLocked] = useState(0);
     const [totalUnlocked, setTotalUnlocked] = useState(0);
     const [totalWithdrawn, setTotalWithdrawn] = useState(0);
@@ -37,61 +41,84 @@ export const Withdraw = ({ data }: Props) => {
         }
     };
 
+    const activate = async () => {
+        await activateProvider();
+    };
+
     return (
-        <div className="w-full flex flex-col items-center justify-between bg-black-700 rounded-[30px] inset-shadow px-8 py-6">
-            <div className="w-full flex justify-center font-kanit-medium color-gray-gradient text-shadow text-lg tracking-[.12em]">
-                WITHDRAW
-            </div>
-            <div className="w-full flex flex-col items-center my-6">
-                <div className="flex flex-col text-gray-600">
-                    <div>
-                        <span className="text-4xl tracking-[.1em]">
-                            {Math.trunc(totalLocked)}
-                        </span>
-                        <span className="tracking-[.1em]">
-                            {Math.abs(totalLocked - Math.trunc(totalLocked))
-                                .toFixed(2)
-                                .slice(1)}
-                        </span>
-                    </div>
-                    <span className="text-sm tracking-[.1em] mt-1">
-                        TOTAL LOCKED
-                    </span>
+        <div className="w-full bg-black-700 rounded-[30px] inset-shadow relative">
+            <div
+                className={`w-full flex flex-col items-center justify-between px-8 py-6 ${
+                    account ? '' : 'blur-sm'
+                }`}
+            >
+                <div className="w-full flex justify-center font-kanit-medium color-gray-gradient text-shadow text-lg tracking-[.12em]">
+                    WITHDRAW
                 </div>
-                <Line />
-                <div className="flex items-center justify-center">
-                    <img
-                        className="w-[3rem] h-[3rem] mr-2"
-                        src={logo}
-                        alt="logo"
-                    ></img>
-                    <div className="flex flex-col">
+                <div className="w-full flex flex-col items-center my-6">
+                    <div className="flex flex-col text-gray-600">
                         <div>
-                            <span className="text-4xl tracking-[.1em] color-gradient">
-                                {Math.trunc(totalUnlocked)}
+                            <span className="text-4xl tracking-[.1em]">
+                                {Math.trunc(totalLocked)}
                             </span>
-                            <span className="tracking-[.1em] color-gradient">
-                                {Math.abs(
-                                    totalUnlocked - Math.trunc(totalUnlocked),
-                                )
+                            <span className="tracking-[.1em]">
+                                {Math.abs(totalLocked - Math.trunc(totalLocked))
                                     .toFixed(2)
                                     .slice(1)}
                             </span>
                         </div>
-                        <span className="text-sm tracking-[.1em] text-gray-600">
-                            TOTAL UNLOCKED
+                        <span className="text-sm tracking-[.1em] mt-1">
+                            TOTAL LOCKED
                         </span>
                     </div>
+                    <Line />
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="w-[3rem] h-[3rem] mr-2"
+                            src={logo}
+                            alt="logo"
+                        ></img>
+                        <div className="flex flex-col">
+                            <div>
+                                <span className="text-4xl tracking-[.1em] color-gradient">
+                                    {Math.trunc(totalUnlocked)}
+                                </span>
+                                <span className="tracking-[.1em] color-gradient">
+                                    {Math.abs(
+                                        totalUnlocked -
+                                            Math.trunc(totalUnlocked),
+                                    )
+                                        .toFixed(2)
+                                        .slice(1)}
+                                </span>
+                            </div>
+                            <span className="text-sm tracking-[.1em] text-gray-600">
+                                TOTAL UNLOCKED
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full flex flex-col items-center justify-center my-6">
+                    <GradientButton
+                        size="small"
+                        disabled={false}
+                        text={`WITHDRAW ${totalUnlocked - totalWithdrawn} LAKE`}
+                        onClick={onWithdrawClick}
+                    />
                 </div>
             </div>
-            <div className="w-full flex flex-col items-center justify-center my-6">
-                <GradientButton
-                    size="small"
-                    disabled={false}
-                    text={`WITHDRAW ${totalUnlocked - totalWithdrawn} LAKE`}
-                    onClick={onWithdrawClick}
-                />
-            </div>
+            {!account && (
+                <div className="absolute top-[50%] left-[6%]">
+                    <GradientButtonWithIcon
+                        size="medium"
+                        disabled={false}
+                        text="CONNECT WALLET"
+                        onClick={activate}
+                    >
+                        <img src={keyIcon} alt="key"></img>
+                    </GradientButtonWithIcon>
+                </div>
+            )}
         </div>
     );
 };
