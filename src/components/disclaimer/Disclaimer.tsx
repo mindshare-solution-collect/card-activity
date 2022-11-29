bc3ab6822cfcef3386e7f7041afeb82471499e6e
VladStarostenko <vlad.starostenko@data-lake.co>
2022-11-29 16:19:33 +0100

    Update BG image for disclaimer

--- src/components/disclaimer/Disclaimer.tsx
+++ src/components/disclaimer/Disclaimer.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const Disclaimer = ({ onAcceptClick }: Props) => (
     <div className="w-full h-[100vh]">
         <div className="w-full h-full z-40 fixed flex items-center justify-center">
-            <div className="w-[30rem] h-[30rem] flex justify-between p-8 bg-black-700 rounded-[20px] inset-shadow overflow-auto">
+            <div className="w-[30rem] h-[30rem] flex justify-between p-8 bg-black-800 rounded-[20px] inset-shadow overflow-auto">
                 <div className="w-full flex flex-col items-center">
                     <div className="w-full flex relative items-center justify-center">
                         <span className="text-3xl color-gray-gradient text-center mt-2 mb-4 tracking-[.1em] font-medium font-kanit-medium cursor-default">
@@ -64,7 +64,7 @@ export const Disclaimer = ({ onAcceptClick }: Props) => (
                 </div>
             </div>
         </div>
-        <BackgroundImage className="w-full h-full flex justify-center items-center m-auto blur-[3px] opacity-40" />
+        <BackgroundImage className="w-full h-full flex justify-center items-center m-auto blur-[3px] opacity-20" />
     </div>
 );
 
