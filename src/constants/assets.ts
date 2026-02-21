49540622a87cf6fbe073aebecbfd26c4a405b677
Igor <romanchaa997@gmail.com>
2026-02-21 22:17:45 +0200

    fix: add ASSET_USDC definition and update all references

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -19,10 +19,16 @@ export const ASSET_DAI: IAsset = {
     decimals: 6,
 };
 
+export const ASSET_USDC: IAsset = {
+    name: 'USD Coin',
+    symbol: 'USDC',
+    decimals: 6,
+};
+
 export const ASSET_WETH: IAsset = {
     name: 'Wrapped Ether',
     symbol: 'WETH',
     decimals: 18,
 };
 
-export const ASSETS: IAsset[] = [ASSET_LAKE, ASSET_ETH, ASSET_USDT, ASSET_WETH];
+export const ASSETS: IAsset[] = [ASSET_LAKE, ASSET_ETH, ASSET_USDC, ASSET_WETH];
